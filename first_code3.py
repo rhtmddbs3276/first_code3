5a8db99510489746ffd21bd89e97afc58e26824c
rhtmddbs3276@gmail.com <rhtmddbs3276@gmail.com>
2024-10-30 10:28:00 +0900

    내용 수정

--- first_code3.py
+++ first_code3.py
@@ -1,6 +1,4 @@
-# 1부터 10까지 합을 계산
-total = 0
-for i in range(1, 11):  # range(1, 11)은 1부터 10까지의 범위를 의미
-    total += i
+print ("hello world")
+print ("This is the first code")
 
-print(f"1부터 10까지의 합: {total}")
+print ("Developer B")
